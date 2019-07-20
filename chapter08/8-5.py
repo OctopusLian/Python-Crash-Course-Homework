f0e6b8bbbdbfa473ccb66333cf6b657b8a0e71d1
OctopusLian <zoctopus@qq.com>
2019-07-20 23:27:17 +0800

    补漏一题

---
+++ chapter08/8-5.py
@@ -0,0 +1,6 @@
+def describe_city(cityname,contury='china'):
+    print(cityname.title + " is in " + contury.title)
+
+describe_city('beijing')
+describe_city('NewYork','American')
+describe_city('chengdu')
